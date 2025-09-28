a77b458fa0d7550f33ae96c629194c96421911a0
Dongwoo <devonshin@hotmail.com>
2025-09-28 22:27:00 +0200

    8.8 향상된 에이전트 기능 및 사용자 경험을 제공합니다.
    에이전트의 도구 구성을 업데이트하고 사용자 인터페이스를 개선하여 사용자 경험을 향상시킵니다. Yahoo Finance MCP 서버를 통합하여 금융 관련 정보 접근성을 높이고, 이미지 업로드 기능을 개선합니다.
    Mise à jour de la configuration des outils de l'agent et amélioration de l'interface utilisateur pour améliorer l'expérience utilisateur. Intégration du serveur Yahoo Finance MCP pour améliorer l'accès aux informations financières et amélioration de la fonctionnalité de téléchargement d'images.

--- chatgpt-clone/main.py
+++ chatgpt-clone/main.py
@@ -1,65 +1,26 @@
-import base64
-
 import dotenv
 
 dotenv.load_dotenv()
-import time
-import asyncio
-import streamlit as st
-from agents import (Agent, Runner, SQLiteSession, WebSearchTool, FileSearchTool, ImageGenerationTool,
-                    CodeInterpreterTool, HostedMCPTool)
 from openai import OpenAI
+import asyncio
+import base64
+import streamlit as st
+from agents import (
+    Agent,
+    Runner,
+    SQLiteSession,
+    WebSearchTool,
+    FileSearchTool,
+    ImageGenerationTool,
+    CodeInterpreterTool,
+    HostedMCPTool,
+)
+from agents.mcp.server import MCPServerStdio
 
 client = OpenAI()
-VECTOR_STORE_ID = "vs_68d91090f5808191a127939ee78b28a7"
 
-if "agent" not in st.session_state:
-    st.session_state["agent"] = Agent(
-        name="ChatGPT Clone",
-        model="gpt-4o-mini",
-        instructions="""
-        당신은 유용한 조수입니다.
-        다음 도구에 액세스할 수 있습니다:
-            - Web Search Tool: 사용자가 학습 데이터에 없는 질문을 할 때 이 도구를 사용하세요. 사용자가 현재 또는 미래의 이벤트에 대해 질문할 때 이 도구를 사용하여 답을 모른다고 생각되면 먼저 웹에서 검색해 보세요.
-            - File Search Tool: 사용자가 자신과 관련된 사실에 대해 질문할 때 이 도구를 사용합니다. 또는 특정 파일에 대해 질문할 때 사용합니다.
-            - Image generation tool: 사용자가 이미지 생성을 요청할 때 이 도구를 사용합니다.
-            - Code Interpreter tool: 코드 실행이 필요할 때 이 도구를 사용합니다. 
-        """,
-        tools=[
-            WebSearchTool(),
-            FileSearchTool(
-                vector_store_ids=[VECTOR_STORE_ID],
-                max_num_results=3
-            ),
-            ImageGenerationTool(
-                tool_config={
-                    "type": "image_generation",
-                    "quality": "medium",
-                    "size": "1024x1024",
-                    "output_format": "jpeg",
-                    "partial_images": 3
-                }
-            ),
-            CodeInterpreterTool(
-                tool_config={
-                    "type": "code_interpreter",
-                    "container": {
-                        "type": "auto"
-                    }
-                }
-            ),
-            HostedMCPTool(
-                tool_config={
-                    "server_url": "https://mcp.context7.com/mcp",
-                    "server_label": "Context7",
-                    "type": "mcp",
-                    "server_description": "Use this to get the docs from software projects.",
-                    "require_approval": "never",
-                }
-            )
-        ],
-    )
-agent = st.session_state["agent"]
+VECTOR_STORE_ID = "vs_68a0815f62388191a9c3701ceb237234"
+
 
 if "session" not in st.session_state:
     st.session_state["session"] = SQLiteSession(
@@ -110,6 +71,7 @@ async def paint_history():
 
 
 def update_status(status_container, event):
+
     status_messages = {
         "response.web_search_call.completed": ("✅ Web search completed.", "complete"),
         "response.web_search_call.in_progress": ("🔍 Starting web search...","running",),
@@ -137,47 +99,112 @@ def update_status(status_container, event):
         status_container.update(label=label, state=state)
 
 
-asyncio.run(paint_history())
-
-
 async def run_agent(message):
-    with st.chat_message("ai"):
-        status_container = st.status("⏳", expanded=False)
-        code_placeholder = st.empty()
-        image_placeholder = st.empty()
-        text_placeholder = st.empty()
-        response = ""
-        code_response = ""
+    yfinance_server = MCPServerStdio(
+        params={
+            "command": "uvx",
+            "args": ["mcp-yahoo-finance"],
+        },
+        cache_tools_list=True,
+    )
 
-        # 세션에 저장해서 새로운 유저 인풋이 있을 경우에 삭제한다.
-        st.session_state["code_placeholder"] = code_placeholder
-        st.session_state["image_placeholder"] = image_placeholder
-        st.session_state["text_placeholder"] = text_placeholder
+    async with yfinance_server:
 
-        stream = Runner.run_streamed(
-            agent,
-            message,
-            session=session,
+        agent = Agent(
+            mcp_servers=[
+                yfinance_server,
+            ],
+            name="ChatGPT Clone",
+            instructions="""
+        You are a helpful assistant.
+
+        You have access to the followign tools:
+            - Web Search Tool: Use this when the user asks a questions that isn't in your training data. Use this tool when the users asks about current or future events, when you think you don't know the answer, try searching for it in the web first.
+            - File Search Tool: Use this tool when the user asks a question about facts related to themselves. Or when they ask questions about specific files.
+            - Code Interpreter Tool: Use this tool when you need to write and run code to answer the user's question.
+        """,
+            tools=[
+                WebSearchTool(),
+                FileSearchTool(
+                    vector_store_ids=[VECTOR_STORE_ID],
+                    max_num_results=3,
+                ),
+                ImageGenerationTool(
+                    tool_config={
+                        "type": "image_generation",
+                        "quality": "high",
+                        "output_format": "jpeg",
+                        "partial_images": 1,
+                    }
+                ),
+                CodeInterpreterTool(
+                    tool_config={
+                        "type": "code_interpreter",
+                        "container": {
+                            "type": "auto",
+                        },
+                    }
+                ),
+                HostedMCPTool(
+                    tool_config={
+                        "server_url": "https://mcp.context7.com/mcp",
+                        "type": "mcp",
+                        "server_label": "Context7",
+                        "server_description": "Use this to get the docs from software projects.",
+                        "require_approval": "never",
+                    }
+                ),
+            ],
         )
 
-        async for event in stream.stream_events():
-            if event.type == "raw_response_event":
-                update_status(status_container, event.data.type)
-                if event.data.type == "response.output_text.delta":
-                    response += event.data.delta
-                    text_placeholder.write(response.replace("$", "\\$"))
-                elif event.data.type == "response.image_generation_call.partial_image":
-                    image = base64.b64decode(event.data.partial_image_b64)
-                    image_placeholder.image(image)
-                elif event.data.type == "response.code_interpreter_call_code.delta":
-                    code_response += event.data.delta
-                    code_placeholder.code(code_response)
+        with st.chat_message("ai"):
+            status_container = st.status("⏳", expanded=False)
+            code_placeholder = st.empty()
+            image_placeholder = st.empty()
+            text_placeholder = st.empty()
+            response = ""
+            code_response = ""
+
+            st.session_state["code_placeholder"] = code_placeholder
+            st.session_state["image_placeholder"] = image_placeholder
+            st.session_state["text_placeholder"] = text_placeholder
+
+            stream = Runner.run_streamed(
+                agent,
+                message,
+                session=session,
+            )
+
+            async for event in stream.stream_events():
+                if event.type == "raw_response_event":
+
+                    update_status(status_container, event.data.type)
+
+                    if event.data.type == "response.output_text.delta":
+                        response += event.data.delta
+                        text_placeholder.write(response.replace("$", "\$"))
+
+                    if event.data.type == "response.code_interpreter_call_code.delta":
+                        code_response += event.data.delta
+                        code_placeholder.code(code_response)
+
+                    elif (
+                        event.data.type
+                        == "response.image_generation_call.partial_image"
+                    ):
+                        image = base64.b64decode(event.data.partial_image_b64)
+                        image_placeholder.image(image)
 
 
 prompt = st.chat_input(
     "Write a message for your assistant",
     accept_file=True,
-    file_type=["txt", "jpeg", "jpg", "png"],
+    file_type=[
+        "txt",
+        "jpg",
+        "jpeg",
+        "png",
+    ],
 )
 
 if prompt:
@@ -189,44 +216,50 @@ if prompt:
     if "text_placeholder" in st.session_state:
         st.session_state["text_placeholder"].empty()
 
-    # 파일을 먼저 로드 시킨다
     for file in prompt.files:
         if file.type.startswith("text/"):
             with st.chat_message("ai"):
-                with st.status("⏳Uploading file..") as status:
+                with st.status("⏳ Uploading file...") as status:
                     uploaded_file = client.files.create(
                         file=(file.name, file.getvalue()),
-                        purpose="user_data"
+                        purpose="user_data",
                     )
-                    status.update(label="⏳Attaching file..")
+                    status.update(label="⏳ Attaching file...")
                     client.vector_stores.files.create(
                         vector_store_id=VECTOR_STORE_ID,
                         file_id=uploaded_file.id,
                     )
-                    status.update(label="✔ File uploaded.", state="complete")
+                    status.update(label="✅ File uploaded", state="complete")
         elif file.type.startswith("image/"):
-            with st.status("⏳Uploading image..") as status:
+            with st.status("⏳ Uploading image...") as status:
                 file_bytes = file.getvalue()
-                base64_bytes = base64.b64encode(file_bytes).decode("utf-8")
-                data_uri = f"data:{file.type};base64,{base64_bytes}"
+                base64_data = base64.b64encode(file_bytes).decode("utf-8")
+                data_uri = f"data:{file.type};base64,{base64_data}"
                 asyncio.run(
-                    session.add_items([{
-                        "role": "user",
-                        "content": [{
-                            "type": "input_image",
-                            "detail": "auto",
-                            "image_url": data_uri
-                        }],
-                    }])
+                    session.add_items(
+                        [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "input_image",
+                                        "detail": "auto",
+                                        "image_url": data_uri,
+                                    }
+                                ],
+                            }
+                        ]
+                    )
                 )
-                status.update(label="✔ Image uploaded.", state="complete")
-            with st.chat_message("user"):
+                status.update(label="✅ Image uploaded", state="complete")
+            with st.chat_message("human"):
                 st.image(data_uri)
 
     if prompt.text:
         with st.chat_message("human"):
             st.write(prompt.text)
         asyncio.run(run_agent(prompt.text))
+
 
 with st.sidebar:
     reset = st.button("Reset memory")
